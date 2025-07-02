c6e7ff2da3a94052f65031290c19d2a195ff27c1
rudraarora <rudra.arora.ipu@hmail.com>
2025-07-03 00:49:01 +0530

    Eleventh Commit

--- frontend/react-app/src/Home.jsx
+++ frontend/react-app/src/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navigation from './Navigation'
 import { useNavigate } from "react-router"
+import { ToastContainer } from 'react-toastify'
 const Home = () => {
 
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <div className="flex justify-center items-center h-screen">
         <h1 className="text-5xl font-bold text-amber-50">Welcome to My Shop</h1>
       </div>
+      <ToastContainer autoClose={2000}/>
     </div>
   )
 }
